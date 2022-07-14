d0079a1fb13bf295ee098530b1e05ba99e47b4aa
fuujazz <crfuujack@gmail.com>
2022-07-14 15:58:16 +0300

    new stylings

--- src/pages/IsBireyselArsivTek.js
+++ src/pages/IsBireyselArsivTek.js
@@ -1,129 +1,156 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { Link, Navigate, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Link, Navigate, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { MDBDataTable } from 'mdbreact';
-import { MainContext, useContext } from '../contex'
-import SideBarLinks from '../components/SideBarLinks';
-import AlacakGoster from '../components/AlacakGoster';
-import VerecekGoster from '../components/VerecekGoster';
-
-
-
+import { MDBDataTable } from "mdbreact";
+import { MainContext, useContext } from "../contex";
+import SideBarLinks from "../components/SideBarLinks";
+import AlacakGoster from "../components/AlacakGoster";
+import VerecekGoster from "../components/VerecekGoster";
 
 export default function IsBireyselArsivTek(props) {
-    const navigate = useNavigate();
-    const [tableData2,setTableData2] = useState({});
-    const[verecekData,setVerecekData] = useState({});
-    
-    const{verecekId,setVerecekId,erisimKodu} = useContext(MainContext);
-    const{isId,isTuru,setIsTuru} = useContext(MainContext);
-   
-    const sil = async () => {
-        const response = await fetch("http://127.0.0.1:5000/is/bireysel/sil/",{
-            method:"POST",
-            mode:'cors',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                erisimKodu:erisimKodu,
-                isId:isId
-   
-            })
-        })
+	const navigate = useNavigate();
+	const [tableData2, setTableData2] = useState({});
+	const [verecekData, setVerecekData] = useState({});
 
-        const returnVAL = await response.json();
-        console.log(returnVAL.durum);
-        
-    }
-    
+	const { verecekId, setVerecekId, erisimKodu } = useContext(MainContext);
+	const { isId, isTuru, setIsTuru } = useContext(MainContext);
 
-    const silClick = () => {
-        if(window.confirm("Firma Silinecek Emin Misiniz?") == true){
-            sil();
-            navigate("/is/bireysel");
-        }
-    }
+	const sil = async () => {
+		const response = await fetch("http://127.0.0.1:5000/is/bireysel/sil/", {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				erisimKodu: erisimKodu,
+				isId: isId,
+			}),
+		});
 
-    const guncelle = () => {
-        navigate("/is/bireysel/guncelle");
-    }
+		const returnVAL = await response.json();
+		console.log(returnVAL.durum);
+	};
 
-    const goruntule = () => {
+	const silClick = () => {
+		if (window.confirm("Firma Silinecek Emin Misiniz?") == true) {
+			sil();
+			navigate("/is/bireysel");
+		}
+	};
 
-        navigate("/is/bireysel/musteri");
-    }
-    const clickVerecek =  (verecekId) => {
-        setVerecekId(verecekId);
-        navigate("/is/bireysel/arsiv/tek");
-    };
+	const guncelle = () => {
+		navigate("/is/bireysel/guncelle");
+	};
 
+	const goruntule = () => {
+		navigate("/is/bireysel/musteri");
+	};
+	const clickVerecek = (verecekId) => {
+		setVerecekId(verecekId);
+		navigate("/is/bireysel/arsiv/tek");
+	};
 
-    useEffect(() => {
-        setIsTuru(0);
-    },[])
+	useEffect(() => {
+		setIsTuru(0);
+	}, []);
 
-    return (
-    <div>
-        {/* navbar */}
-        <div className="navbar bg-base-100 shadow">
-            <div className="flex-none">
-             
-                <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
-            </div>
-            <div className="flex-1">
-                <a href="/home" className="btn btn-ghost normal-case text-xl">Biçerer Sigorta</a>
-            </div>
-            <div className="flex-none">
-            <a className='btn btn-error hover:text-white' href="/logout">Çıkış Yap</a>
-            </div>
-        </div>
-        <div className="drawer ">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content w-screen h-screen flex flex-column  align-center">
-                 {/* Toggle Button */}
+	return (
+		<div>
+			{/* navbar */}
+			<div className="navbar bg-base-100 shadow">
+				<div className="flex-none">
+					<label
+						htmlFor="my-drawer"
+						className="btn btn-square btn-ghost drawer-button"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="inline-block w-5 h-5 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</label>
+				</div>
+				<div className="flex-1">
+					<a href="/home" className="btn btn-ghost normal-case text-xl">
+						Biçerer Sigorta
+					</a>
+				</div>
+				<div className="flex-none">
+					<a className="btn btn-error hover:text-white" href="/logout">
+						Çıkış Yap
+					</a>
+				</div>
+			</div>
+			<div className="drawer ">
+				<input id="my-drawer" type="checkbox" className="drawer-toggle" />
+				<div className="drawer-content w-screen h-screen flex flex-column  align-center">
+					{/* Toggle Button */}
 
-                <div className="w-100 h-100 d-flex justify-content-center align-items-start  h-25">
-                    <div className="d-flex flex-column justify-content-center align-items-center w-75 h-100  rounded">
-                        <div className="text-center my-5">
-                            <h1><b style={{'fontSize':'30px'}}>Yapmak Istediginiz islemi seciniz:</b></h1>
-                        </div>
-                    <div className="d-flex justify-content-center">
-                        <a onClick={goruntule} className='btn bg-green-500 text-black hover:bg-green-300 hover:text-white mx-2'>Musteriye Yapilan Isleri Gor</a>
-                        <a onClick={guncelle} className='btn bg-blue-500 text-black hover:bg-blue-300 hover:text-white mx-2'>Güncelle</a>
-                        <a onClick={silClick} className='btn bg-red-500 text-black hover:bg-red-300 hover:text-white mx-2'>Sil</a>
-                    </div>
+					<div className="w-100 h-100 d-flex justify-content-center align-items-start  h-25">
+						<div className="d-flex flex-column justify-content-center align-items-center w-75 h-100  rounded">
+							<div className="text-center my-5">
+								<h1>
+									<b style={{ fontSize: "30px" }}>
+										Yapmak Istediginiz islemi seciniz:
+									</b>
+								</h1>
+							</div>
+							<div className="d-flex justify-content-center">
+								<a
+									onClick={goruntule}
+									className="btn btn-success text-black hover:btn-ghost rounded mx-2"
+								>
+									Musteriye Yapilan Isleri Gor
+								</a>
+								<a
+									onClick={guncelle}
+									className="btn btn-info hover:btn-ghost rounded mx-2"
+								>
+									Güncelle
+								</a>
+								<a
+									onClick={silClick}
+									className="btn bg-danger hover:btn-ghost rounded mx-2"
+								>
+									Sil
+								</a>
+							</div>
 
-                   <div className="d-flex justify-content-center align-items-center mt-5">
-                            <div className="d-flex flex-column justify-content-center align-items-center mt-4 mr-5">
-                                <Link to="/alacak/ekle" className='btn bg-gray-200 text-dark'>Alacak ekle</Link>
-                                <AlacakGoster isTuru={0} />
-                            </div>
-                        
-                            <div className="d-flex flex-column justify-content-center align-items-center mt-4">
-                                <Link to="/verecek/ekle" className='btn bg-gray-200 text-dark'>Verecek ekle</Link>
-                                <VerecekGoster isTuru={0} />
-                            </div>
-                </div>
-                    </div>
-                   
-            
-                   
+							<div className="d-flex justify-content-center align-items-center mt-5">
+								<div className="d-flex flex-column justify-content-center align-items-center mt-4 mr-5">
+									<Link to="/alacak/ekle" className="btn btn-outline rounded">
+										Alacak ekle
+									</Link>
+									<AlacakGoster isTuru={0} />
+								</div>
 
-                </div>
-                
-            </div> 
-            <div className="drawer-side ">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-
-                <SideBarLinks></SideBarLinks>
-                
-                </ul>
-            </div>  
-        </div>
-
-    </div>
-  )
+								<div className="d-flex flex-column justify-content-center align-items-center mt-4">
+									<Link to="/verecek/ekle" className="btn btn-outline rounded">
+										Verecek ekle
+									</Link>
+									<VerecekGoster isTuru={0} />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="drawer-side ">
+					<label htmlFor="my-drawer" className="drawer-overlay"></label>
+					<ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+						<SideBarLinks></SideBarLinks>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/MusteriGuncelle.js
+++ src/pages/MusteriGuncelle.js
@@ -8,177 +8,204 @@ import SideBarLinks from "../components/SideBarLinks";
 import { useNavigate } from "react-router-dom";
 
 export default function MusteriGuncelle(props) {
-  const navigate = useNavigate();
-  const { musteriId, setMusteriId, musteriData, setMusteriData ,erisimKodu} =
-    useContext(MainContext);
-  
+	const navigate = useNavigate();
+	const { musteriId, setMusteriId, musteriData, setMusteriData, erisimKodu } =
+		useContext(MainContext);
 
-  let defaultDate = null;
-  const initialData = {
-    erisimKodu: erisimKodu,
-    musteriId: musteriId,
-    musteriAdi: musteriData.ad,
-    musteriSoyadi: musteriData.soyad,
-    musteriTc: musteriData.tc,
-    musteriTelefon: musteriData.telefon,
-    musteriDogumTarihi: musteriData.dogumTarihi,
-    musteriMailAdresi: musteriData.mailAdresi,
-  };
+	let defaultDate = null;
+	const initialData = {
+		erisimKodu: erisimKodu,
+		musteriId: musteriId,
+		musteriAdi: musteriData.ad,
+		musteriSoyadi: musteriData.soyad,
+		musteriTc: musteriData.tc,
+		musteriTelefon: musteriData.telefon,
+		musteriDogumTarihi: musteriData.dogumTarihi,
+		musteriMailAdresi: musteriData.mailAdresi,
+	};
 
-  useEffect(() => {
-    defaultDate = new Date(
-      musteriData.dogumTarihi.split("-")[2] +
-        "-" +
-        musteriData.dogumTarihi.split("-")[1] +
-        "-" +
-        musteriData.dogumTarihi.split("-")[0]
-    );
-  }, []);
+	useEffect(() => {
+		defaultDate = new Date(
+			musteriData.dogumTarihi.split("-")[2] +
+				"-" +
+				musteriData.dogumTarihi.split("-")[1] +
+				"-" +
+				musteriData.dogumTarihi.split("-")[0]
+		);
+	}, []);
 
-  const update = async () => {
-    const response = await fetch("http://127.0.0.1:5000/musteri/guncelle/", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(initialData),
-    });
-  };
+	const update = async () => {
+		const response = await fetch("http://127.0.0.1:5000/musteri/guncelle/", {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(initialData),
+		});
+	};
 
-  const guncelle = () => {
-    update();
-    navigate("/musteriler");
-  };
+	const guncelle = () => {
+		update();
+		navigate("/musteriler");
+	};
 
-  return (
-    <div>
-      {/* navbar */}
-      <div className="navbar bg-base-100 shadow">
-        <div className="flex-none">
-          <label
-            htmlFor="my-drawer"
-            className="btn btn-square btn-ghost drawer-button"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </label>
-        </div>
-        <div className="flex-1">
-          <a href="/home" className="btn btn-ghost normal-case text-xl">
-            Biçerer Sigorta
-          </a>
-        </div>
-        <div className="flex-none">
-          <a className="btn btn-error hover:text-white" href="/logout">
-            Çıkış Yap
-          </a>
-        </div>
-      </div>
-      <div className="drawer ">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content w-screen h-screen flex flex-column  align-center">
-          {/* Toggle Button */}
+	return (
+		<div>
+			{/* navbar */}
+			<div className="navbar bg-base-100 shadow">
+				<div className="flex-none">
+					<label
+						htmlFor="my-drawer"
+						className="btn btn-square btn-ghost drawer-button"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="inline-block w-5 h-5 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</label>
+				</div>
+				<div className="flex-1">
+					<a href="/home" className="btn btn-ghost normal-case text-xl">
+						Biçerer Sigorta
+					</a>
+				</div>
+				<div className="flex-none">
+					<a className="btn btn-error hover:text-white" href="/logout">
+						Çıkış Yap
+					</a>
+				</div>
+			</div>
+			<div className="drawer ">
+				<input id="my-drawer" type="checkbox" className="drawer-toggle" />
+				<div className="drawer-content w-screen h-screen flex flex-column  align-center">
+					{/* Toggle Button */}
 
-          <div
-          
-            className="container d-flex flex-column align-items-center bg-gray-200 mt-3 rounded"
-          >
-            <div
-              style={{ height: "25%", "fontSize": "30px" }}
-              className="w-50 p-0 bg-warning rounded d-flex justify-content-center align-items-center mt-3"
-            >
-              Brans Adi
-            </div>
+					<div className="container d-flex flex-column align-items-center  mt-3 rounded">
+						<h1
+							style={{ fontSize: "30px" }}
+							className="border-y-2 border-indigo-500 flex justify-center py-4  w-1/4 mt-10 mb-4"
+						>
+							Müşteri Güncelle
+						</h1>
 
-            <div className="form d-flex flex-column align-items-center w-100 mt-3">
-              <div className="d-flex flex-column justify-content-center align-items-start w-100">
-                <label htmlhtmlFor="">Musteri Adi:</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    defaultValue={musteriData.ad}
-                    onChange={(e) => {
-                    initialData.musteriAdi = e.target.value;
-                    }}
-                />
-              </div>
-             
-              <label htmlhtmlFor="">Soyad</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue={musteriData.soyad}
-                onChange={(e) => {
-                  initialData.musteriSoyadi = e.target.value;
-                }}
-              />
-              <br />
-              <label htmlhtmlFor="">Tc</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue={musteriData.tc}
-                onChange={(e) => {
-                  initialData.musteriTc = e.target.value;
-                }}
-              />
-              <br />
-              <label htmlhtmlFor="">Telefon</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue={musteriData.telefon}
-                onChange={(e) => {
-                  initialData.musteriTelefon = e.target.value;
-                }}
-              />
-              <br />
+						<div className="form d-flex flex-column align-items-center w-100 mt-3">
+							<div className="flex justify-center w-1/2">
+								<label htmlhtmlFor="" className="input-group">
+									<span className="w-40 flex justify-center">Müşteri Adı</span>
+									<input
+										type="text"
+										className="form-control"
+										defaultValue={musteriData.ad}
+										onChange={(e) => {
+											initialData.musteriAdi = e.target.value;
+										}}
+									/>
+								</label>
+							</div>
 
-              <label htmlhtmlFor="">Musteri Dogum Tarihi</label>
-              <input
-                type="date"
-                className="form-control"
-                defaultValue={defaultDate}
-                onChange={(e) => {
-                  initialData.musteriDogumTarihi = e.target.value;
-                }}
-              />
-              <br />
-              <label htmlhtmlFor="">Musteri Mail Adresi</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue={initialData.musteriMailAdresi}
-                onChange={(e) => {
-                  initialData.musteriMailAdresi = e.target.value;
-                }}
-              />
-              <br />
-              <button className="btn bg-green-200" onClick={guncelle}>
-                Guncelle
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="drawer-side ">
-          <label htmlFor="my-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-            <SideBarLinks></SideBarLinks>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+							<div className="flex justify-center w-1/2 mt-3">
+								<label htmlFor="" className="input-group">
+									<span className="w-40 flex justify-center">Soyad</span>
+									<input
+										type="text"
+										className="form-control"
+										defaultValue={musteriData.soyad}
+										onChange={(e) => {
+											initialData.musteriSoyadi = e.target.value;
+										}}
+									/>
+								</label>
+
+								<br />
+							</div>
+
+							<div class="flex justify-center w-1/2 mt-3">
+								<label htmlFor="" className="input-group">
+									<span className="w-40 flex justify-center">Tc</span>
+									<input
+										type="text"
+										className="form-control"
+										defaultValue={musteriData.tc}
+										onChange={(e) => {
+											initialData.musteriTc = e.target.value;
+										}}
+									/>
+								</label>
+							</div>
+							<br />
+
+							<div class="flex justify-center w-1/2 ">
+								<label htmlFor="" className="input-group">
+									<span className="w-40 flex justify-center">Telefon</span>
+									<input
+										type="text"
+										className="form-control"
+										defaultValue={musteriData.telefon}
+										onChange={(e) => {
+											initialData.musteriTelefon = e.target.value;
+										}}
+									/>
+								</label>
+							</div>
+							<br />
+
+							<div class="flex justify-center w-1/2 ">
+								<label htmlFor="" className="input-group">
+									<span className="w-40 flex justify-center">Doğum Tarihi</span>
+									<input
+										type="date"
+										className="form-control"
+										defaultValue={defaultDate}
+										onChange={(e) => {
+											initialData.musteriDogumTarihi = e.target.value;
+										}}
+									/>
+								</label>
+							</div>
+
+							<br />
+
+							<div class="flex justify-center w-1/2 ">
+								<label htmlFor="" className="input-group">
+									<span class="w-40 flex justify-center">Email</span>
+									<input
+										type="text"
+										className="form-control"
+										defaultValue={initialData.musteriMailAdresi}
+										onChange={(e) => {
+											initialData.musteriMailAdresi = e.target.value;
+										}}
+									/>
+								</label>
+							</div>
+
+							<br />
+							<button
+								className="btn btn-success rounded mb-5"
+								onClick={guncelle}
+							>
+								Guncelle
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="drawer-side ">
+					<label htmlFor="my-drawer" className="drawer-overlay"></label>
+					<ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+						<SideBarLinks></SideBarLinks>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
