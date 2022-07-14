b2ca58ddfd80a9ff40c3efbb18cc42bc8babc9dd
fuujazz <crfuujack@gmail.com>
2022-07-14 16:56:47 +0300

    styling

--- src/pages/IsOrtakArsivGoster.js
+++ src/pages/IsOrtakArsivGoster.js
@@ -7,195 +7,201 @@ import { useNavigate } from "react-router-dom";
 import SideBarLinks from "../components/SideBarLinks";
 
 export default function IsOrtakArsivGoster() {
+	const navigate = useNavigate();
+	const {
+		arsivId,
+		musteriId,
+		setMusteriId,
+		isId,
+		setIsId,
+		erisimKodu,
+		isTuru,
+		setIsTuru,
+	} = useContext(MainContext);
 
-    const navigate = useNavigate();
-    const {arsivId,musteriId,setMusteriId,isId,setIsId,erisimKodu,isTuru,setIsTuru} = useContext(MainContext);
+	const [fetchedData, setFetchedData] = useState([]);
+	const click = (id, musteriId) => {
+		setMusteriId(musteriId);
+		setIsId(id);
+		setIsTuru(0);
 
-    const [fetchedData,setFetchedData] = useState([]);
-    const click =  (id,musteriId) => {
-        setMusteriId(musteriId);
-        setIsId(id);
-        setIsTuru(0);
-  
-        navigate("/is/ortak/arsiv/tek");
-    };
+		navigate("/is/ortak/arsiv/tek");
+	};
 
+	const fetchData = async () => {
+		const response = await fetch(
+			"http://127.0.0.1:5000/is/ortak/arsiv/goster/hepsi/",
+			{
+				method: "POST",
+				mode: "cors",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					erisimKodu: erisimKodu,
+					arsivId: arsivId,
+				}),
+			}
+		);
 
-    const fetchData = async () => {
-        const response = await fetch("http://127.0.0.1:5000/is/ortak/arsiv/goster/hepsi/",{
-            method:"POST",
-            mode:"cors",
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                erisimKodu: erisimKodu,
-                arsivId: arsivId
+		const returnData = await response.json();
+		console.log("return data ");
+		console.log(returnData);
+		const processedData = [];
+		for (let i = 1; i < Array.from(returnData.keys()).length; i++) {
+			processedData.push({
+				musteriAdi: returnData[i].musteriAdi,
+				bransAdi: returnData[i].bransAdi,
+				sigortaSirketiAdi: returnData[i].sigortaSirketiAdi,
+				arsivKlasoruAdi: returnData[i].arsivKlasoruAdi,
+				plaka: returnData[i].plaka,
+				ruhsatSeriNo: returnData[i].ruhsatSeriNo,
+				policeNo: returnData[i].policeNo,
+				policeBitisTarihi: returnData[i].policeBitisTarihi,
+				komisyonOraniFirma: returnData[i].komisyonOraniFirma,
+				firmaAdi: returnData[i].firmaAdi,
 
+				clickEvent: () => click(returnData[i].id, returnData[i].musteriId),
+			});
+		}
 
-            })
-        })
-        
-        const returnData = await response.json();
-        console.log("return data ");
-        console.log(returnData);
-        const processedData = [];
-        for(let i = 1; i<Array.from(returnData.keys()).length;i++){
-            processedData.push({
-                musteriAdi: returnData[i].musteriAdi,
-                bransAdi :returnData[i].bransAdi,
-                sigortaSirketiAdi: returnData[i].sigortaSirketiAdi,
-                arsivKlasoruAdi: returnData[i].arsivKlasoruAdi,
-                plaka: returnData[i].plaka,
-                ruhsatSeriNo: returnData[i].ruhsatSeriNo,
-                policeNo: returnData[i].policeNo,
-                policeBitisTarihi: returnData[i].policeBitisTarihi,
-                komisyonOraniFirma:returnData[i].komisyonOraniFirma,
-                firmaAdi:returnData[i].firmaAdi,
-                
-                clickEvent: () => click(returnData[i].id,returnData[i].musteriId)
-            });
+		const data = {
+			columns: [
+				{
+					label: "Musteri Adi",
+					field: "musteriAdi",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Brans Adi",
+					field: "bransAdi",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Firma Adi",
+					field: "firmaAdi",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Sigorta Sirketi",
+					field: "sigortaSirketiAdi",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Arsiv",
+					field: "arsivKlasoruAdi",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Plaka",
+					field: "plaka",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Ruhsat Seri No",
+					field: "ruhsatSeriNo",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Police No",
+					field: "policeNo",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Firma Komisyon Orani",
+					field: "komisyonOraniFirma",
+					sort: "asc",
+					width: 150,
+				},
+				{
+					label: "Police Bitis Tarihi",
+					field: "policeBitisTarihi",
+					sort: "asc",
+					width: 150,
+				},
+			],
+			rows: processedData,
+		};
 
-            
-        }
+		await setFetchedData(data);
+		console.log("fetchlenmis data");
+		console.log(data);
+	};
 
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-        const data = {
-            columns:[
-                {
-                    label: 'Musteri Adi',
-                    field: 'musteriAdi',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Brans Adi',
-                    field: 'bransAdi',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Firma Adi',
-                    field: 'firmaAdi',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Sigorta Sirketi',
-                    field: 'sigortaSirketiAdi',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Arsiv',
-                    field: 'arsivKlasoruAdi',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Plaka',
-                    field: 'plaka',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Ruhsat Seri No',
-                    field: 'ruhsatSeriNo',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Police No',
-                    field: 'policeNo',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Firma Komisyon Orani',
-                    field: 'komisyonOraniFirma',
-                    sort: 'asc',
-                    width: 150
-                },
-                {
-                    label: 'Police Bitis Tarihi',
-                    field: 'policeBitisTarihi',
-                    sort: 'asc',
-                    width: 150
-                }
-               
-            ],
-            rows: processedData
-        }
+	return (
+		<div>
+			{/* navbar */}
+			<div className="navbar bg-base-100 shadow">
+				<div className="flex-none">
+					<label
+						htmlFor="my-drawer"
+						className="btn btn-square btn-ghost drawer-button"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="inline-block w-5 h-5 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</label>
+				</div>
+				<div className="flex-1">
+					<a href="/home" className="btn btn-ghost normal-case text-xl">
+						Biçerer Sigorta
+					</a>
+				</div>
+				<div className="flex-none">
+					<a className="btn btn-error hover:text-white" href="/logout">
+						Çıkış Yap
+					</a>
+				</div>
+			</div>
+			<div className="drawer">
+				<input id="my-drawer" type="checkbox" className="drawer-toggle" />
+				<div className="drawer-content">
+					{/* Toggle Button */}
 
-        await setFetchedData(data);
-        console.log("fetchlenmis data");
-        console.log(data);
+					<div className="container my-5">
+						<div className="flex justify-center align-center">
+							<h1>
+								<b style={{ fontSize: "30px" }}>Ortak Isler</b>
+							</h1>
+						</div>
+						<div className="d-flex justify-content-center align-items-center mt-3">
+							<a href="/is/ortak/ekle" className="btn btn-success rounded mx-2">
+								Ekle
+							</a>
+						</div>
 
-    };
-
-    
-    useEffect(() => {
-        fetchData();
-    },[])
-    
-
-
-    return (
-    <div>
-        {/* navbar */}
-        <div className="navbar bg-base-100 shadow">
-            <div className="flex-none">
-             
-                <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
-            </div>
-            <div className="flex-1">
-               <a href="/home" className="btn btn-ghost normal-case text-xl">Biçerer Sigorta</a>
-            </div>
-            <div className="flex-none">
-               <a className='btn btn-error hover:text-white' href="/logout">Çıkış Yap</a>
-            </div>
-        </div>
-        <div className="drawer">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-                 {/* Toggle Button */}
-
-                <div className="container my-5">
-                    <div className="flex justify-center align-center">
-                        <h1>
-                            <b style={{'fontSize':'30px'}}>Ortak Isler</b>
-                        </h1>
-                    </div>
-                    <div className="d-flex justify-content-center align-items-center mt-3">
-                        <a href="/is/ortak/ekle" className='btn bg-green-400 text-black hover:bg-green-300 hover:text-white mx-2'>Ekle</a>
-                    </div>
-
-                    <MDBDataTable
-                        striped
-                        bordered
-                        hover
-                    
-                        data={fetchedData}
-                        />
-
-                </div>
-            
-
-                
-                 
-            </div> 
-            <div className="drawer-side ">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-
-                <SideBarLinks></SideBarLinks>
-                
-                </ul>
-            </div>  
-        </div>
-      
-      
-    </div>
-  )
-
+						<MDBDataTable striped bordered hover data={fetchedData} />
+					</div>
+				</div>
+				<div className="drawer-side ">
+					<label htmlFor="my-drawer" className="drawer-overlay"></label>
+					<ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+						<SideBarLinks></SideBarLinks>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/VerecekEkle.js
+++ src/pages/VerecekEkle.js
@@ -1,157 +1,149 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-import { MDBDataTable } from 'mdbreact';
-import { MainContext, useContext } from '../contex'
-import SideBarLinks from '../components/SideBarLinks';
+import { MDBDataTable } from "mdbreact";
+import { MainContext, useContext } from "../contex";
+import SideBarLinks from "../components/SideBarLinks";
 import { useNavigate } from "react-router-dom";
 
 export default function VerecekEkle(props) {
-    const navigate = useNavigate();
-    const [counter,setCounter] = useState(0);
-    const {token,userId,setArsivId,isTuru,isId,erisimKodu} = useContext(MainContext);
-    const initialData  = {
-        erisimKodu: erisimKodu
+	const navigate = useNavigate();
+	const [counter, setCounter] = useState(0);
+	const { token, userId, setArsivId, isTuru, isId, erisimKodu } =
+		useContext(MainContext);
+	const initialData = {
+		erisimKodu: erisimKodu,
+	};
+	console.log(isTuru);
 
-    }
-    console.log(isTuru);
-   
+	const ekle = async () => {
+		const response = await fetch("http://127.0.0.1:5000/verecekler/ekle/", {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(initialData),
+		});
+	};
 
-    const ekle = async () => {
-        const response = await fetch("http://127.0.0.1:5000/verecekler/ekle/",{
-            method:"POST",
-            mode:"cors",
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(initialData)
-        })       
-    };
+	useEffect(() => {
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, "0");
+		var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+		var yyyy = today.getFullYear();
 
+		today = yyyy + "-" + mm + "-" + dd;
+		initialData["tarih"] = today;
 
+		initialData["isTuru"] = isTuru;
+		initialData["isId"] = isId;
 
-useEffect(() => {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+		console.log(initialData);
+		console.log(isTuru);
+	}, []);
 
-    today = yyyy + '-' + mm + '-' + dd;
-    initialData["tarih"] = today
+	const ekleClick = () => {
+		ekle();
+		if (isTuru === 0) {
+			navigate("/is/bireysel/arsiv/tek");
+		} else if (isTuru === 1) {
+			navigate("/is/ortak/arsiv/tek");
+		}
+	};
 
-    initialData["isTuru"] = isTuru;
-    initialData["isId"] = isId;
+	return (
+		<div>
+			{/* navbar */}
+			<div className="navbar bg-base-100 shadow">
+				<div className="flex-none">
+					<label
+						htmlFor="my-drawer"
+						className="btn btn-square btn-ghost drawer-button"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="inline-block w-5 h-5 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</label>
+				</div>
+				<div className="flex-1">
+					<a href="/home" className="btn btn-ghost normal-case text-xl">
+						Biçerer Sigorta
+					</a>
+				</div>
+				<div className="flex-none">
+					<a className="btn btn-error hover:text-white" href="/logout">
+						Çıkış Yap
+					</a>
+				</div>
+			</div>
+			<div className="drawer ">
+				<div className="drawer-content w-screen h-screen flex flex-column  align-center">
+					{/* Toggle Button */}
 
-    console.log(initialData);
-    console.log(isTuru);
+					<div
+						style={{ height: "100%" }}
+						className="container mx-auto my-5 flex flex-col  items-center border-2 "
+					>
+						<h1
+							style={{ fontSize: "30px" }}
+							className="border-y-2 border-indigo-500 flex justify-center py-2  w-1/4 mt-10"
+						>
+							Verecek Ekle
+						</h1>
 
- },[]);
+						<div className="form d-flex flex-column align-items-center mt-5">
+							<label htmlFor="aciklama" className="input-group">
+								<span className="w-32 flex justify-center">Açıklama</span>
+								<input
+									className="w-48 input input-sm input-secondary"
+									onChange={(e) => {
+										initialData["aciklama"] = e.target.value;
+									}}
+									type="text"
+									name="aciklama"
+								/>
+							</label>
+							<br />
 
+							<label htmlFor="miktar" className="input-group">
+								<span className="w-32 flex justify-center">Miktar</span>
+								<input
+									className="w-48 input input-sm input-secondary"
+									onChange={(e) => {
+										initialData["miktar"] = e.target.value;
+									}}
+									type="number"
+									name="miktar"
+								/>
+							</label>
+							<br />
 
-
- const ekleClick = () => {
-    ekle();
-    if(isTuru === 0){
-        navigate("/is/bireysel/arsiv/tek");
-
-    }
-    else if(isTuru === 1){
-        navigate("/is/ortak/arsiv/tek");
-
-    }
- }
-
-
-
-    return (
-    <div>
-        {/* navbar */}
-        <div className="navbar bg-base-100 shadow">
-            <div className="flex-none">
-             
-                <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
-            </div>
-            <div className="flex-1">
-                <a href="/home" className="btn btn-ghost normal-case text-xl">Biçerer Sigorta</a>
-            </div>
-            <div className="flex-none">
-            <a className='btn btn-error hover:text-white' href="/logout">Çıkış Yap</a>
-            </div>
-        </div>
-        <div className="drawer ">
-            
-            <div className="drawer-content w-screen h-screen flex flex-column  align-center">
-                 {/* Toggle Button */}
-
-                <div style={{'height':'100%'}} className="container mx-auto my-5 flex flex-col  items-center border-2 bg-gray-200">
-                   
-                <div style={{"height":"15%",'fontSize':'30px'}} className="w-100 bg-success rounded d-flex justify-content-center align-items-center">Bireysel Is Ekle</div>
-                
-                   <div className="form d-flex flex-column align-items-center">
-
-
-                        <label htmlFor="aciklama">Aciklama</label>
-                        <input onChange={(e) => {initialData["aciklama"] = e.target.value }} type="text" name="aciklama"  />
-                        <br />
-
-                        <label htmlFor="miktar">Miktar</label>
-                        <input onChange={(e) => {initialData["miktar"] = e.target.value }} type="number" name="miktar"  />
-                        <br />
-
-
-
-                      
-
-                        {/* <label htmlFor="branslar">Branslar</label>
-                        <select  onChange={(e) => {initialData["bransId"] = e.target.value }}  id="branslar" name="branslar">
-                            
-                            {
-                            
-                                    fetchedData["branslar"].map((branslar)=>{
-                                        initialData["bransId"] = branslar["id"];
-                                        return (<option key={branslar["id"]} value={"" + branslar["id"]}>{branslar["ad"]}</option>)
-                                    })
-                                
-                            }  
-                        </select> */}
-
-                      
-                 
-                        {/* <label htmlFor="policeNo">Police No</label>
-                        <input onChange={(e) => {initialData["policeNo"] = e.target.value }} type="text" name="policeNo" placeholder='Police No Giriniz:' />
-                        <br />
-                            
-                        <label htmlFor="policeBitisTarihi">Police Bitis Tarih</label>
-                        <input onChange={(e) => {initialData["policeBitisTarihi"] = e.target.value }} type="date" name="policeBitisTarihi"  />
- */}
-
-
-
-
-
-                    
-                    {/* <button onClick={() => {ekle()}} className='btn bg-green-200' >Ekle</button> */}
-                    <button onClick={ekleClick} className='btn bg-green-200' >Ekle</button>
-                          
-                            
-                   </div>
-
-                </div>
-               
-                
-                 
-            </div> 
-            <div className="drawer-side ">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-
-                <SideBarLinks></SideBarLinks>
-                
-                </ul>
-            </div>  
-        </div>
-
-    </div>
-  )
+							{/* <button onClick={() => {ekle()}} className='btn bg-green-200' >Ekle</button> */}
+							<button onClick={ekleClick} className="btn btn-success rounded">
+								Ekle
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="drawer-side ">
+					<label htmlFor="my-drawer" className="drawer-overlay"></label>
+					<ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+						<SideBarLinks></SideBarLinks>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
